b01a6df13bee1dd91f66caf34340a2afb1bb1a37
Alex Nichol <unixpickle@gmail.com>
2022-02-21 14:20:50 -0800

    add docs

--- weather_electric/scripts/dump_aggregate.py
+++ weather_electric/scripts/dump_aggregate.py
@@ -1,3 +1,31 @@
+"""
+Combine PG&E electric usage with corresponding historical weather data in a
+single CSV file.
+
+Before running the script, you should download your hourly usage data from PG&E
+using the Green Button.
+
+You also need to setup an API key on https://www.worldweatheronline.com/. This
+API key must be passed via the --api_key flag, e.g. `--api_key XXXX`.
+
+Finally, you must figure out a way to identify your location to the weather
+API. This can be a latitude,longitude pair. Pass the location via the
+`--location` flag, e.g. `--location 35.9828,-121.1231`.
+
+Finaly usage of the script might look like:
+
+    python -m weather_electric.dump_aggregate \
+        --api_key XXXX \
+        --location 35.9828,-121.1231 \
+        /path/to/pge_data.csv \
+        aggregate_output.csv
+
+The script will show a loading indicator as it fetches weather data for each
+day in the PG&E data file. Additionally, it will create a `weather_cache/`
+sub-directory in the current working directory so that future invocations will
+not have to re-fetch historical weather data.
+"""
+
 import argparse
 import csv
 
@@ -17,14 +45,14 @@ def main():
 
     api_client = WeatherAPI(api_key=args.api_key)
     electric_data = read_green_button_csv_file(args.pge_data)
-    aggregate = aggregate_usage(api_client, tqdm(electric_data), args.location)
+    aggregate = aggregate_usage(api_client, electric_data, args.location)
 
     with open(args.output_path, "w") as f:
         writer = csv.DictWriter(
             f, fieldnames=sorted(UsageAndWeather.empty().to_csv_row().keys())
         )
         writer.writeheader()
-        writer.writerows(x.to_csv_row() for x in aggregate)
+        writer.writerows(x.to_csv_row() for x in tqdm(aggregate))
 
 
 if __name__ == "__main__":
